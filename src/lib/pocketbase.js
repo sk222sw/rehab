fcec0d140bd7e778f5730dedc70392668a368bdd
Sonny Kjellberg <sonnykjellberg@gmail.com>
2025-08-08 13:29:24 +0200

    make log clearer

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-console.log(import.meta.env.VITE_PB_URL)
+console.log("PB_URL", import.meta.env.VITE_PB_URL)
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 
 export default pb;
